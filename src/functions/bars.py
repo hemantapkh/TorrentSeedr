c726b1c32d6d0bdc70841e2156798569ec77bcd9
Hemanta Pokharel <hemantapkh@yahoo.com>
2021-06-26 19:25:59 +0545

    Add bars.py

---
+++ src/functions/bars.py
@@ -0,0 +1,11 @@
+#: Progress bar of downloading torrents
+def progressBar(progress):
+    bars = int(float(progress)) // 5
+
+    return f"{'█'*bars}{(20-bars)*'░'} {round(float(progress), 2)}%"
+
+#: Account space bar
+def spaceBar(totalSpace, spaceUsed):
+    bars = round((spaceUsed / totalSpace) * 20)
+
+    return f"{'█'*bars}{(20-bars)*'░'}"
