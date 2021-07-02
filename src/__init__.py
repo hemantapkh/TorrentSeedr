f106e0ac64d59d64ac2f36bd63802a15480fdcd8
Hemanta Pokharel <hemantapkh@yahoo.com>
2021-07-02 20:30:53 +0545

    Add __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,12 @@
+from src.objs import config, dbSql
+from src.objs import bot, language
+from src.commands.start import start
+from src.commands.files import files
+from src.commands.active import active 
+from src.commands.account import account
+from src.commands.getLink import getLink
+from src.commands.getFiles import getFiles
+from src.commands.addTorrent import addTorrent
+from src.commands.removeFile import removeFile
+from src.commands.deleteFolder import deleteFolder
+from src.commands.cancelDownload import cancelDownload
