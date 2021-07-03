5fb385f36ace3af18b2e63c8d4a02266fadd776a
Hemanta Pokharel <hemantapkh@yahoo.com>
2021-07-03 23:22:18 +0545

    Add removeFile.py

---
+++ src/commands/removeFile.py
@@ -0,0 +1,29 @@
+from src.objs import *
+from src.functions.exceptions import exceptions, noAccount
+
+# Remove files
+@bot.message_handler(func=lambda message: message.text and message.text[:8] == '/remove_')
+def removeFile(message):
+    userId = message.from_user.id
+    userLanguage = dbSql.getSetting(userId, 'language')
+    ac = dbSql.getDefaultAc(userId)
+
+    #! If user has account
+    if ac:
+        sent = bot.send_message(message.chat.id, language['removingFile'][userLanguage])
+        id = message.text[8:]
+
+        account = Seedr(cookie=ac['cookie'])
+
+        response = account.deleteFile(id).json()
+
+        #!? If file removed successfully
+        if response['result'] == True:
+            bot.edit_message_text(text=language['removedSuccessfully'][userLanguage], chat_id=message.chat.id, message_id=sent.id)
+        
+        else:
+            exceptions(message, response, userLanguage)
+        
+    #! If no accounts
+    else:
+        noAccount(message, userLanguage)
