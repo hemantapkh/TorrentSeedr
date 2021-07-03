edb89f72570866ea26bc4730405b21ff348425c1
Hemanta Pokharel <hemantapkh@yahoo.com>
2021-07-03 23:26:07 +0545

    Add cancelDownload.py

---
+++ src/commands/cancelDownload.py
@@ -0,0 +1,28 @@
+from src.objs import *
+from src.functions.exceptions import exceptions, noAccount
+
+# Cancel active downloads
+@bot.message_handler(func=lambda message: message.text and message.text[:8] == '/cancel_')
+def cancelDownload(message):
+    userId = message.from_user.id
+    userLanguage = dbSql.getSetting(userId, 'language')
+    ac = dbSql.getDefaultAc(userId)
+
+    #! If user has account
+    if ac:
+        account = Seedr(cookie=ac['cookie'])
+
+        sent = bot.send_message(message.chat.id, language['cancelling'][userLanguage])
+        id = message.text[8:]
+        response = account.deleteTorrent(id).json()
+
+        #!? If torrent cancelled successfully
+        if response['result'] == True:
+            bot.edit_message_text(text=language['cancelled'][userLanguage], chat_id=message.chat.id, message_id=sent.id)
+
+        else:
+            exceptions(message, response, userLanguage)
+        
+    #! If no accounts
+    else:
+        noAccount(message, userLanguage)
