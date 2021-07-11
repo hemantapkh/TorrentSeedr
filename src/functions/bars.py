ca1ca5b3f3b4595f5a440cd6ce62a26ae7be3037
Hemanta Pokharel <hemantapkh@yahoo.com>
2021-07-11 18:39:53 +0545

    Change bar characters

--- src/functions/bars.py
+++ src/functions/bars.py
@@ -1,11 +1,11 @@
 #: Progress bar of downloading torrents
 def progressBar(progress):
-    bars = int(float(progress)) // 5
+    bars = int(float(progress)) // 6
 
-    return f"{'▣'*bars}{(20-bars)*'▢'} {round(float(progress), 2)}%"
+    return f"{'▬'*bars}{(15-bars)*'▭'} {round(float(progress), 2)}%"
 
 #: Account space bar
 def spaceBar(totalSpace, spaceUsed):
     bars = round((spaceUsed / totalSpace) * 20)
 
-    return f"{'█'*bars}{(20-bars)*'░'}"
+    return f"{'▣'*bars}{(20-bars)*'▢'}"
