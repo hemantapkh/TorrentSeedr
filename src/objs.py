4b3a7871faa2d3d2b5782ce1deacd9ba8d82e29d
Hemanta Pokharel <hemantapkh@yahoo.com>
2021-07-14 14:15:33 +0545

    Change directory on deployment

--- src/objs.py
+++ src/objs.py
@@ -6,13 +6,8 @@ import telebot
 from models import dbQuery
 from seedr import Seedr, Account
 
-#! Finding the absolute path of the config file
-scriptPath = path.abspath(__file__)
-dirPath = path.dirname(scriptPath)
-configPath = path.join(dirPath,'config.json')
-
 seedrAc = Account()
-config = json.load(open(configPath))
+config = json.load(open('src/config.json'))
 language = json.load(open(config['language']))
 dbSql = dbQuery(config['database'])
 bot = telebot.TeleBot(config['botToken'], parse_mode='HTML')
