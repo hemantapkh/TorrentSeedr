d04fdee97d2caf136b88440aa7fa9ef943920099
Hemanta Pokharel <hemantapkh@yahoo.com>
2021-08-26 08:19:46 +0545

    Hide github oauth if already done

--- src/commands/featureUnavailable.py
+++ src/commands/featureUnavailable.py
@@ -2,4 +2,8 @@ from src.objs import *
 from src.functions.keyboard import githubAuthKeyboard
 
 def featureUnavailable(message, userLanguage):
-    bot.send_message(message.chat.id, language['featureNotAvailable'][userLanguage]+'\n\n'+language['getFreeSpace'][userLanguage], reply_markup=githubAuthKeyboard(userLanguage))
+    userId = message.from_user.id
+    if dbSql.getSetting(userId, 'githubId') == '0':
+        bot.send_message(message.chat.id, language['featureNotAvailable'][userLanguage]+'\n\n'+language['getFreeSpace'][userLanguage], reply_markup=githubAuthKeyboard(userLanguage))
+    else:
+        bot.send_message(message.chat.id, language['featureNotAvailable'][userLanguage])
