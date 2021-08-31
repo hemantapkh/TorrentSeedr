3eef4a59bcc21cc06bb7c7f67683d48f54b1ffb4
Hemanta Pokharel <hemantapkh@yahoo.com>
2021-08-31 19:17:28 +0545

    Fix error in callbacks

--- src/functions/floodControl.py
+++ src/functions/floodControl.py
@@ -11,20 +11,21 @@ def floodControl(message, userLanguage):
     #! If the user is not banned
     if not dbSql.getSetting(userId, 'blockTill', table='flood') - int(time.time())  > 0:
         lastMessage = dbSql.getSetting(userId, 'lastMessage', table='flood')
+        called = True if type(message) == telebot.types.CallbackQuery else False
 
-        messageDate = message.date
+        messageDate = message.message.date if called else message.date
 
         #! Spam detected
         if messageDate - lastMessage < 1:
             #! If the user is already warned, block for 5 minutes
             if dbSql.getSetting(userId, 'warned', table='flood'):
-                bot.send_message(message.chat.id, language['blockedTooFast'][userLanguage])
+                bot.send_message(message.message.chat.id if called else message.chat.id, language['blockedTooFast'][userLanguage])
                 dbSql.setSetting(userId, 'blockTill', int(time.time())+300, table='flood')
                 dbSql.setSetting(userId, 'warned', 0, table='flood')
             
             #! If the user is not warned, warn for the first time
             else:
-                bot.send_message(message.chat.id, language['warningTooFast'][userLanguage])
+                bot.send_message(message.message.chat.id if called else message.chat.id, language['warningTooFast'][userLanguage])
                 dbSql.setSetting(userId, 'warned', 1, table='flood')
             
             return False
