e8e5a5f84ee347dc93c89d645147bd4fa78fb40f
Hemanta Pokharel <hemantapkh@yahoo.com>
2021-11-20 20:55:28 +0545

    Fix /login command

--- src/functions/referralCode.py
+++ src/functions/referralCode.py
@@ -8,11 +8,14 @@ def referralCode():
     if users:
         index = 0 if not previousUser else users.index(previousUser) + 1 if (len(users)-1) > users.index(previousUser) else 0
         account = dbSql.getDefaultAc(users[index])
-        
-        previousUser = users[index]
-        dbSql.setSetting(account['ownerId'], 'totalRefer', dbSql.getSetting(account['ownerId'], 'totalRefer')+1)
 
-        return account['accountId'] if account else '921385'
+        if account: 
+            previousUser = users[index]
+            dbSql.setSetting(account['ownerId'], 'totalRefer', dbSql.getSetting(account['ownerId'], 'totalRefer')+1)
+
+            return account['accountId']
+        else:
+            return '921385'
     
     else:
         return '921385'
