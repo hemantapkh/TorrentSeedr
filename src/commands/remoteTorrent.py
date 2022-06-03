ace87643bd4abf7e38cfbd1659a25b5ee1f1d343
Hemanta Pokharel <Hemantapkh@yahoo.com>
2022-06-03 16:21:26 +0545

    Remove flood control in remote torrent

--- src/commands/remoteTorrent.py
+++ src/commands/remoteTorrent.py
@@ -11,30 +11,29 @@ async def remoteTorrent(message):
     userId = message.from_user.id
     userLanguage = dbSql.getSetting(userId, 'language')
 
-    if floodControl(message, userLanguage):
-        ac = dbSql.getDefaultAc(userId)
+    ac = dbSql.getDefaultAc(userId)
 
-        #! If user has an account
-        if ac:
-            account = Seedr(
-                token=ac['token'],
-                callbackFunc=lambda token: dbSql.updateAccount(
-                    token, userId, ac['accountId']
-                )
+    #! If user has an account
+    if ac:
+        account = Seedr(
+            token=ac['token'],
+            callbackFunc=lambda token: dbSql.updateAccount(
+                token, userId, ac['accountId']
             )
+        )
 
-            response = account.scanPage(message.text)
+        response = account.scanPage(message.text)
 
-            if 'error' not in response:
-                if response['torrents']:
-                    await asyncio.gather(addTorrent(message, userLanguage, magnetLink=response['torrents'][0]['magnet']))
-
-                else:
-                    bot.send_message(message.chat.id, language["noTorrentInURL"][userLanguage])
+        if 'error' not in response:
+            if response['torrents']:
+                await asyncio.gather(addTorrent(message, userLanguage, magnetLink=response['torrents'][0]['magnet']))
 
             else:
-                exceptions(message, response, ac, userLanguage)
+                bot.send_message(message.chat.id, language["noTorrentInURL"][userLanguage])
 
-        #! If no accounts
         else:
-            noAccount(message, userLanguage)
+            exceptions(message, response, ac, userLanguage)
+
+    #! If no accounts
+    else:
+        noAccount(message, userLanguage)
