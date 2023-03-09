6d86615f75e72ba8e251154d107c8f456db7370e
Hemanta Pokharel <hemanta.pokharel@docsumo.com>
2023-03-09 11:51:23 +0545

    Use pm2 instead of screen

--- .github/workflows/deployScript.sh
+++ .github/workflows/deployScript.sh
@@ -1,4 +1,4 @@
 # Due to a bug in appleboy/ssh-action@master, some commands shows error and fails to execute.
 # So, running a bash file instead of commands
 
-echo $1 > /opt/TorrentSeedr/TorrentSeedr/src/config.json  && pkill -f torrentseedr.py ; source /opt/TorrentSeedr/venv/bin/activate && cd /opt/TorrentSeedr/TorrentSeedr && pip install -r requirements.txt && screen -dm python3 torrentseedr.py
+echo $1 > /opt/TorrentSeedr/TorrentSeedr/src/config.json ; source /opt/TorrentSeedr/venv/bin/activate && cd /opt/TorrentSeedr/TorrentSeedr && pip install -r requirements.txt && pm2 restart TorrentSeedr
